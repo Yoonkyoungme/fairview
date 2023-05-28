4ddd176488e125ea5bcf39f94efa39e008f2c02d
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2023-05-28 14:53:20 +0900

    Feat: create an axios instance

---
+++ src/services/index.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// Axios 인스턴스 생성
+const api = axios.create({
+  baseURL: process.env.REACT_APP_BASEURL,
+  headers: {
+    "Content-Type": "application/json",
+    withCredentials: true,
+  },
+});
+
+// 네트워크 연결 상태 확인 함수
+const checkNetworkStatus = async () => {
+  try {
+    await api.get("/"); // 빈 요청을 보내서 서버와의 연결을 확인
+    console.log("네트워크 연결이 정상입니다.");
+  } catch (error) {
+    console.error("네트워크 연결에 문제가 있습니다:", error);
+  }
+};
+
+checkNetworkStatus();
+
+export default api;
